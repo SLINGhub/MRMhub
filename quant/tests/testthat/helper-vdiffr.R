93a6494ee1f5351c60a7d10b7ab662708d1f021e
Bo Burla <bo.burla@gmail.com>
2025-11-10 23:04:17 +0800

    fix depreciated arg

--- quant/tests/testthat/helper-vdiffr.R
+++ quant/tests/testthat/helper-vdiffr.R
@@ -1,9 +1,9 @@
 # quant/tests/testthat/helper-vdiffr.R
 
-expect_doppelganger_cond <- function(title, fig, path = NULL) {
+expect_doppelganger_cond <- function(title, fig) {
   if (Sys.getenv("RUN_VDIFFR", unset = "true") != "true") {
     testthat::skip("Skipping vdiffr test: RUN_VDIFFR != 'true'")
   } else {
-    vdiffr::expect_doppelganger(title = title, fig = fig, path = path)
+    vdiffr::expect_doppelganger(title = title, fig = fig)
   }
 }
